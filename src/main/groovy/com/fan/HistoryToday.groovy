1fee7be80987a7fc45a27e85df60d94987fcd439
fankaiqiang@feinno.com <Fjklqd2018>
2018-09-07 18:20:59 +0800

    历史上的今天爬虫准备

---
+++ src/main/groovy/com/fan/HistoryToday.groovy
@@ -0,0 +1,9 @@
+package com.fan
+
+import com.fission.source.httpclient.ApiLibrary
+
+class HistoryToday extends ApiLibrary{
+    static void main(String[] args) {
+        def url = "http://tools.***.com/his/1201_c.js"
+    }
+}
